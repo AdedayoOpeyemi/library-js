122920f3e53fe03b2215707f329fed4179188ba6
410AngelaVu <andjela1993@gmail.com>
2021-03-23 22:08:32 +0100

    Added loop and Bootstrap card

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 
+const book1 = new Books ('Harry Potter', 'J.K.Rowling', 350, 'read');
+myLibrary.push(book1);
+
+const book2 = new Books ("Little Red Riding Hood", 'H.KAnderson', 15, 'no read');
+myLibrary.push(book2);
+
 function Books(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -15,4 +21,15 @@ function addBookToLibrary(title, author, pages, read) {
   if (book) {
     myLibrary.push(book)
   }
+}
+
+myLibrary.forEach(myBook);
+
+
+
+function myBook(item) {
+  document.getElementById('card-title').innerHTML = item.title;
+  document.getElementById('auth').innerHTML = item.author;
+  document.getElementById('number').innerHTML = item.pages;
+  document.getElementById('read').innerHTML = item.read;
 }
