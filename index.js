88c21b6a8ee0083ecfe51c2cd29fa8b195fb0e2c
410AngelaVu <andjela1993@gmail.com>
2021-03-24 20:24:18 +0100

    Added Book Class

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
-let myLibrary = [];
-
-const book1 = new Books ('Harry Potter', 'J.K.Rowling', 350, 'read');
-myLibrary.push(book1);
-
-const book2 = new Books ("Little Red Riding Hood", 'H.KAnderson', 15, 'no read');
-myLibrary.push(book2);
-
-function Books(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function() {
-   return title + ' by ' + author + ', ' + pages + ' ' + read
+// Book class: Represents a Book
+class Book {
+  constructor(title, author, page) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
   }
-}
-function addBookToLibrary(title, author, pages, read) {
-  // do stuff here
-  let book = new Books (title, author, pages, read);
-  if (book) {
-    myLibrary.push(book)
-  }
-}
-
-myLibrary.forEach(myBook);
-
-
-
-// function myBook(item) {
-//   document.getElementById('card-title').innerHTML = item.title;
-//   document.getElementById('auth').innerHTML = item.author;
-//   document.getElementById('number').innerHTML = item.pages;
-//   document.getElementById('read').innerHTML = item.read;
-// }
-
-function myBook(item) {
-  var itm = document.getElementById("card");
-  var cln = itm.cloneNode(true);
-  cln.getElementsByTagName("card-body")[0].document.getElementByTagName('card-title')[0].innerHTML = item.title;
-  cln.getElementById("card-body").document.getElementById('card-title').innerHTML = item.title;
-  cln.getElementById("card-body").document.getElementById('auth').innerHTML = item.author;
-  cln.getElementById("card-body").document.getElementById('number').innerHTML = item.pages;
-  cln.getElementById("card-body").document.getElementById('read').innerHTML = item.read;
-  document.getElementById('card').appendChild(cln);
 }
