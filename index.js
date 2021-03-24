743dfb637f62f2b3208fea8ab0d6eae98a1e6ea2
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-03-24 20:11:39 +0100

    switch approach

--- index.js
+++ index.js
@@ -27,9 +27,20 @@ myLibrary.forEach(myBook);
 
 
 
+// function myBook(item) {
+//   document.getElementById('card-title').innerHTML = item.title;
+//   document.getElementById('auth').innerHTML = item.author;
+//   document.getElementById('number').innerHTML = item.pages;
+//   document.getElementById('read').innerHTML = item.read;
+// }
+
 function myBook(item) {
-  document.getElementById('card-title').innerHTML = item.title;
-  document.getElementById('auth').innerHTML = item.author;
-  document.getElementById('number').innerHTML = item.pages;
-  document.getElementById('read').innerHTML = item.read;
+  var itm = document.getElementById("card");
+  var cln = itm.cloneNode(true);
+  cln.getElementsByTagName("card-body")[0].document.getElementByTagName('card-title')[0].innerHTML = item.title;
+  cln.getElementById("card-body").document.getElementById('card-title').innerHTML = item.title;
+  cln.getElementById("card-body").document.getElementById('auth').innerHTML = item.author;
+  cln.getElementById("card-body").document.getElementById('number').innerHTML = item.pages;
+  cln.getElementById("card-body").document.getElementById('read').innerHTML = item.read;
+  document.getElementById('card').appendChild(cln);
 }
