b505a0b90ea8067780904d4b3488d648c33a3442
410AngelaVu <andjela1993@gmail.com>
2021-03-24 20:24:52 +0100

    Added UI Class

--- index.js
+++ index.js
@@ -6,3 +6,39 @@ class Book {
     this.page = page;
   }
 }
+
+// UI Class: Handle UI Tasks
+class UI {
+  static displayBooks() {
+    const StoredBooks = [
+    {
+      title: 'Harry Potter',
+      author: 'J.K.Rowling',
+      page: '600'
+    },
+    {
+      title: 'Lord Of The Rings',
+      author: 'J.R.R Tolkien',
+      page: '600'
+    }
+    ];
+    const books = StoredBooks;
+
+    books.forEach((book) => addBookToList(book));
+  
+  function addBookToList(book) {
+    const list = document.querySelector('#book-list');
+
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td class='text-center'>${book.title}</td>
+      <td class='text-center'>${book.author}</td>
+      <td class='text-center'>${book.page}</td>
+      <td><a href='#' class='btn btn-danger btn-sm delete'>X</a></td>
+    `;
+
+    list.appendChild(row);
+  }
+  }
+}
